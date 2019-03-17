5cfb0dd489aa4982a3c902065aeefb2fcdcf364d
Senesero <senesero@gmail.com>
2019-03-17 18:47:05 +0100

    Terminada clase testing React

---
+++ 04_coverage/src/common/components/panel/components/header.spec.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { Header } from './header';
+
+describe('common/components/panel/header tests', () => {
+  it('should render as expected when passing required properties', () => {
+    // Arrange
+    const props = {
+      title: 'test title',
+    };
+
+    // Act
+    const component = shallow(
+      // <Header
+      //   {...props}
+      // />
+      <Header
+        title={props.title}
+      />
+    );
+
+    // Assert
+    expect(component).toMatchSnapshot();
+  });
+});
